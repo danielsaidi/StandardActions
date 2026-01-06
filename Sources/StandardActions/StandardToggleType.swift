7a995223ca7f6e402b52685f904e75411793d3f9
Daniel Saidi <daniel.saidi@gmail.com>
2026-01-06 09:20:25 +0100

    Update copyright info

--- Sources/StandardActions/StandardToggleType.swift
+++ Sources/StandardActions/StandardToggleType.swift
@@ -3,7 +3,7 @@
 //  StandardActions
 //
 //  Created by Daniel Saidi on 2025-06-25.
-//  Copyright © 2025 Daniel Saidi. All rights reserved.
+//  Copyright © 2025-2026 Daniel Saidi. All rights reserved.
 //
 
 import SwiftUI
